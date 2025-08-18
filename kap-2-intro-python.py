3a4f26c91cd2ede33b9818b675fd33841d991d1f
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-08-18 16:06:02 +0200

    förberedelse kap 2

---
+++ kap-2-intro-python.py
@@ -0,0 +1,70 @@
+### MATEMATISKA OPERATIONER ###
+
+# print
+
+# +, -, *, /
+
+# modulo, %
+
+# heltalsdivision, //
+
+# upphöjt i, **
+
+### HELTAL OCH FLYTTAL ###
+
+# heltal = integer (int)
+# går snabbare att räkna med
+# representeras med binära tal
+
+# flyttal = float
+# decimaltal, med punkt eftersom att Python är amerikanskt
+
+# går att blanda heltal och flyttal
+
+# läs s 34-35 i boken
+
+### VARIABLER ###
+
+# Refererar till något i datorns minne, som en låda
+# Tilldelar ett värde till variabeln
+
+# Vill ha beskrivande variabelnamn, till skillnad från matematiken
+
+# Vissa namn är förbjudna
+# Får ej börja med siffror, innehålla vissa specialtecken eller vara ett reserverat ord
+
+# Python klarar å, ä och ö
+
+# Håll er till ett språk, engelska eller svencka
+
+# Håll er till en stil, snake_case eller camelCase
+
+# Omvandla mellan flyttal och heltal
+
+# Ändra värde på variabel (+= osv)
+
+### STRÄNGAR ###
+
+# En datatyp
+
+# Strängar som tal och tal som strängar
+
+# Starkt typat: kan inte blanda datatyper i beräkningar
+# Svagt typat: kan blanda datatyper i beräkningar
+# Python är starkt typat
+
+# Slå ihop strängar med + (konkatenera)
+
+# str()
+
+### UTSKRIFTER ###
+
+# Skriva ut flera saker på två sätt
+
+# Skriva ut radbrytningar, citattecken och liknande
+
+### INMATNINGAR ###
+
+# input()
+
+# Omvandla input till tal
