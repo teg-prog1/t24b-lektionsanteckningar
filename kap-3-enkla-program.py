312fc8238a8cc4a2e69919bbf7f3045a468d07aa
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-08-21 12:27:26 +0200

    förberett anteckningar kap 3

---
+++ kap-3-enkla-program.py
@@ -0,0 +1,12 @@
+# Radbrytningar i program
+
+# Kommentarer, vad är en bra kommentar
+
+# Hur ska en uppgift se ut när ni har gjort den?
+
+# Felmeddelanden
+# Exempel:
+#f = open("demofile.txt")
+#print(f.read())
+
+# Tips om w3schools
