a432f69f45f0c50b6b6ed4397809e97fd16b9b03
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-08-22 11:19:18 +0200

    påbörjad genomgång kap 4

--- kap-3-enkla-program.py
+++ kap-3-enkla-program.py
@@ -4,11 +4,12 @@
 
 # Hur ska en uppgift se ut när ni har gjort den?
 
-#namn = input("Vad är ditt namn? ") # låter användaren skriva in sitt namn
+namn = input("Vad är ditt namn? ") # låter användaren skriva in sitt namn
 
 # Felmeddelanden
 # Exempel:
 f = open("demofile.txt")
 print(f.read())
+# Lär er att läsa och tolka felmeddelanden, typ av fel, vilken rad
 
-# Tips om w3schools
+# Tips om w3schools, länk finns i lektionsloggen
--- kap-4-if-else.py
+++ kap-4-if-else.py
@@ -1,11 +1,27 @@
 # if
 # indentering
+ålder = int(input("Hur gammal är du? "))
+#if ålder < 18:
+    #print("Du är inte myndig")
+#if ålder > 25:
+   # print("Du är vuxen på riktigt")
 
 # jämförelser (==,<, >, <=, >=, !=)
+#if ålder == 18:
+ #   print("Hej")
 
 # else
-
 # elif
+
+if ålder == 18:
+    print("Grattis!")
+elif ålder < 20:
+    print("Hello")
+else:
+    print("Hej!")
+
+# Kan man inte ha bara massa if istället för elif?
+
 
 # pass
 
