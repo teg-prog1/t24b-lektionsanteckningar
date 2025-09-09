cbe0ee2bdd12f8a38ca75422c05c9112adb594bf
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-09-09 16:18:22 +0200

    förberett genomgång kapitel 8

---
+++ kap-8-listor-tipplar.py
@@ -0,0 +1,52 @@
+# fyra datatyper hittills, int, float, bool, string
+# string är itererbar
+
+# lista, element
+
+# ordnad
+#index
+
+# går att förändra
+
+# olika datatyper i listor
+
+# loopa igenom lista med for-loop
+
+# Listmetoder ###
+# len()
+
+# append()
+
+# min()
+
+# sort() och sorted()
+
+# andra metoder på w3shools
+
+# lista med listor, matris
+
+# tipplar/ tuples
+
+# oföränderlig
+
+# ordnad precis som listor
+
+# packa in och packa upp tipplar
+
+#######################################################################################################################
+# Läs sida 100-102 "Allting i Python är referenser"
+
+# Referenser i Python
+
+# Skapa ett nytt variabelnamn som refererar till samma lista
+
+# Kopiera en lista
+
+# Platser i minnet som inget refereras till längra tas omhand genom Pythons garbage collect
+
+# slicea listor
+
+# listomfattningar (list comprehensions)
+
+
+
