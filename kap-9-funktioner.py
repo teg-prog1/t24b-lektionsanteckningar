de0ab908bee0cee3fc62837a80f1050318d5f748
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-09-30 08:12:26 +0200

    förberett genomgång om funktioner

---
+++ kap-9-funktioner.py
@@ -0,0 +1,41 @@
+# len() är en funktion
+
+
+# int() är en funktion
+
+# En funktion är ett block kod som har ett "namn"
+
+# Exempel: hälsningsfunktion (tar inte in något och returnerar ingenting)
+
+# Läs om terminologin på s 107-toppen av s 108
+# parameter
+# argument
+# anropa/kalla på funktion
+# ”Parameter = parkeringsplatsen, argument = bilen som parkerar där.”
+
+# Exempel: räkna summa (en funktion som har två parametrar)
+
+# Exempel: variabler i funktionen är lokala
+
+# Exempel: en funktion som returnerar något (bara en sak)
+# Hur använder vi returvärdet (jämför med input())
+
+# Vad är funktioner bra för?
+
+# Göra samma sak flera gånger (exempel: testa om en input är i rätt spann)
+
+# Strukturera kod (exempel: uppgiften resväska)
+
+#####################################################################################################################################
+
+# Om strukturen i ett program
+# Kort exempel: miniyatzy
+
+# Om default-värden på parametrar
+
+# Om att returnera flera saker från en funktion
+
+# Om lokala och globala variabler (läs s 112-113)
+
+# Varför vill vi undvika globala variabler och vilka variabler är rimliga att ha globala?
+
