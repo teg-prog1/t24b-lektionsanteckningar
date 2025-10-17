6c3dc06b283ee32bd3344b0ed02245e84ac754a1
Felicia Rosenberg <felicia.rosenberg@tabyenskilda.se>
2025-10-17 09:26:23 +0200

    anteckningar kapitel 16

--- kap_11_felhantering.py
+++ kap_11_felhantering.py
@@ -4,26 +4,26 @@
 # Vi kan hantera felen med hjälp av try except
 
 # Exempel beräkna procent
-'''def skriv_ut_resultat(antal_rätt,antal_frågor):
+def skriv_ut_resultat(antal_rätt,antal_frågor):
     try:
         procent_rätt = (antal_rätt/antal_frågor)*100
         print(f"Du fick {antal_rätt} rätt och {antal_frågor-antal_rätt} fel av totalt {antal_frågor} frågor.")
         print(f"Det motsvarar {procent_rätt}% rätt.")
     except ZeroDivisionError:
-        print("Det fanns inga besvarade frågor")'''
+        print("Det fanns inga besvarade frågor")
 
 
 # Exempel fråga om tal
-'''ej_svarat = True
+ej_svarat = True
 while ej_svarat:
     try:
         ålder = int(input("Skriv in din ålder: "))
         ej_svarat = False
     except ValueError:
-        print("Du måste skriva in ett heltal")'''
+        print("Du måste skriva in ett heltal")
 
 # Exempel fråga om tal inklusive egna gränser
-'''ej_svarat = True
+ej_svarat = True
 while ej_svarat:
     try:
         ålder = int(input("Skriv in din ålder: "))
@@ -31,10 +31,10 @@ while ej_svarat:
             raise ValueError
         ej_svarat = False
     except ValueError:
-        print("Du måste skriva in ett heltal som är större än eller lika med 0")'''
+        print("Du måste skriva in ett heltal som är större än eller lika med 0")
 
 # Exempel med index, två olika fel!
-'''frukter = ["äpple", "banan", "kiwi"]
+frukter = ["äpple", "banan", "kiwi"]
 
 while True:
     try:
@@ -47,4 +47,4 @@ while True:
     except ValueError:
         print("Fel, du har inte skrivit in ett heltal.")
     except IndexError:
-        print("Fel, du har skrivit ett för stort tal")'''
+        print("Fel, du har skrivit ett för stort tal")
--- kap_16_dictionaries_mängder.py
+++ kap_16_dictionaries_mängder.py
@@ -1,24 +1,44 @@
 # LITE OM STRÄNGAR OCH LISTOR
 
 # list()
+namn = "Felicia Hultén Maltsson!"
+#print(list(namn))
 
 # split()
 # https://www.w3schools.com/python/ref_string_split.asp
+uppdelat = namn.split("l")
+#print(uppdelat)
 
 # join()
 # https://www.w3schools.com/python/ref_string_join.asp 
+myTuple = ("John", "Peter", "Vicky")
+
+x = "#".join(myTuple)
+
+#print(x)
 
 # MÄNGDER (Obs: finns inte i boken)
 # Oordnade och tillåter inte dubletter
+frukter = {"äpple", "kiwi", "banan", "äpple"}
+#print(frukter)
+
+märken = {"äpple", "ferrari", "volvo"}
 
 # Metoder för mängder
 # https://www.w3schools.com/python/python_ref_set.asp 
 
 # difference()
+resultat = frukter.difference(märken)
+#print(resultat)
 
 # intersection()
+resultat_2 = märken.intersection(frukter)
+#print(resultat_2)
 
 # issubset()
+ny_mängd = {"äpple"}
+resultat_3 = ny_mängd.issubset(frukter)
+#print(resultat_3)
 
 # DICTIONARIES
 # Nycklar och värden
@@ -26,28 +46,51 @@
 # Nycklarna måste vara oföränderliga
 # Dictionaries är föränderliga
 
+exempel_dictionary = {"Felicia":26, "Victor":27, "Kamilla":56, "Maximiliam":17}
+
 # Plocka ut värde
+print(exempel_dictionary["Felicia"])
 
 # Lägga till värde
+exempel_dictionary["Frida"]=24
+print(exempel_dictionary["Frida"])
 
 # Ta bort värde
+del exempel_dictionary["Kamilla"]
+print(exempel_dictionary)
 
 # En lista som värde
+land_info = {"Sverige": ["Stockholm", "10 miljoner"], "Norge":["Oslo", "5 miljoner"]}
 
 # Metoder för dictionaries
 # https://www.w3schools.com/python/python_dictionaries_methods.asp 
 # keys()
+nycklar = exempel_dictionary.keys()
+print(nycklar)
 
 # values()
+värden = list(exempel_dictionary.values())
+print(värden)
 
 # items()
+par = exempel_dictionary.items()
+print(par)
 
 # Loopa igenom dictionary
 # Obs: tidigare var inte dictionaries sorterade, det är de nu
 # Dock kan du inte komma åt innehållet med index
 
 # Loopa med for element in
+for element in exempel_dictionary:
+    print(element)
 
 # Loopa med values() eller keys()
+for värde in exempel_dictionary.values():
+    print(värde)
 
 # Loopa med items()
+for element in exempel_dictionary.items():
+    print(element)
+
+for nyckel, värde in exempel_dictionary.items():
+    print(nyckel, värde)
